63c38bf136156b6d5d5909bb1dab51e4f4180df3
ElyasYassin <164119976+ElyasYassin@users.noreply.github.com>
2024-07-08 21:42:48 -0600

    Update gaussian_mlp.py

--- mjrl/policies/gaussian_mlp.py
+++ mjrl/policies/gaussian_mlp.py
@@ -1,8 +1,7 @@
 import numpy as np
-from mjrl.utils.fc_network import FCNetwork
+from mjrl.utils.fc_network import FCNetwork, FCNetworkWithBatchNorm
 import torch
 from torch.autograd import Variable
-
 
 class MLP:
     def __init__(self, env_spec,
@@ -10,53 +9,36 @@ class MLP:
                  min_log_std=-3,
                  init_log_std=0,
                  seed=None):
-        """
-        :param env_spec: specifications of the env (see utils/gym_env.py)
-        :param hidden_sizes: network hidden layer sizes (currently 2 layers only)
-        :param min_log_std: log_std is clamped at this value and can't go below
-        :param init_log_std: initial log standard deviation
-        :param seed: random seed
-        """
         self.n = env_spec.observation_dim  # number of states
         self.m = env_spec.action_dim  # number of actions
         self.min_log_std = min_log_std
 
-        # Set seed
-        # ------------------------
         if seed is not None:
             torch.manual_seed(seed)
             np.random.seed(seed)
 
-        # Policy network
-        # ------------------------
         self.model = FCNetwork(self.n, self.m, hidden_sizes)
-        # make weights small
         for param in list(self.model.parameters())[-2:]:  # only last layer
            param.data = 1e-2 * param.data
         self.log_std = Variable(torch.ones(self.m) * init_log_std, requires_grad=True)
         self.trainable_params = list(self.model.parameters()) + [self.log_std]
 
-        # Old Policy network
-        # ------------------------
         self.old_model = FCNetwork(self.n, self.m, hidden_sizes)
         self.old_log_std = Variable(torch.ones(self.m) * init_log_std)
         self.old_params = list(self.old_model.parameters()) + [self.old_log_std]
         for idx, param in enumerate(self.old_params):
             param.data = self.trainable_params[idx].data.clone()
 
-        # Easy access variables
-        # -------------------------
         self.log_std_val = np.float64(self.log_std.data.numpy().ravel())
         self.param_shapes = [p.data.numpy().shape for p in self.trainable_params]
         self.param_sizes = [p.data.numpy().size for p in self.trainable_params]
         self.d = np.sum(self.param_sizes)  # total number of params
 
-        # Placeholders
-        # ------------------------
         self.obs_var = Variable(torch.randn(self.n), requires_grad=False)
 
-    # Utility functions
-    # ============================================
+    def show_activations(self):
+        return self.model.activations
+
     def get_param_values(self):
         params = np.concatenate([p.contiguous().view(-1).data.numpy()
                                  for p in self.trainable_params])
@@ -70,10 +52,8 @@ class MLP:
                 vals = vals.reshape(self.param_shapes[idx])
                 param.data = torch.from_numpy(vals).float()
                 current_idx += self.param_sizes[idx]
-            # clip std at minimum value
             self.trainable_params[-1].data = \
                 torch.clamp(self.trainable_params[-1], self.min_log_std).data
-            # update log_std_val for sampling
             self.log_std_val = np.float64(self.log_std.data.numpy().ravel())
         if set_old:
             current_idx = 0
@@ -82,12 +62,9 @@ class MLP:
                 vals = vals.reshape(self.param_shapes[idx])
                 param.data = torch.from_numpy(vals).float()
                 current_idx += self.param_sizes[idx]
-            # clip std at minimum value
             self.old_params[-1].data = \
                 torch.clamp(self.old_params[-1], self.min_log_std).data
 
-    # Main functions
-    # ============================================
     def get_action(self, observation):
         o = np.float32(observation.reshape(1, -1))
         self.obs_var.data = torch.from_numpy(o)
@@ -143,3 +120,68 @@ class MLP:
         Dr = 2 * new_std ** 2 + 1e-8
         sample_kl = torch.sum(Nr / Dr + new_log_std - old_log_std, dim=1)
         return torch.mean(sample_kl)
+
+    # Ensure to close the writer when done
+    def close_writer(self):
+        self.model.close_writer()
+        self.old_model.close_writer()
+
+
+
+class BatchNormMLP(MLP):
+    def __init__(self, env_spec,
+                 hidden_sizes=(64,64),
+                 min_log_std=-3,
+                 init_log_std=0,
+                 seed=None,
+                 nonlinearity='relu',
+                 dropout=0,
+                 log_dir='runs/activations_with_batchnorm',
+                 *args, **kwargs):
+        self.n = env_spec.observation_dim  # number of states
+        self.m = env_spec.action_dim  # number of actions
+        self.min_log_std = min_log_std
+
+        if seed is not None:
+            torch.manual_seed(seed)
+            np.random.seed(seed)
+
+        self.model = FCNetworkWithBatchNorm(self.n, self.m, hidden_sizes, nonlinearity, dropout, log_dir=log_dir)
+
+        for param in list(self.model.parameters())[-2:]:  # only last layer
+           param.data = 1e-2 * param.data
+        self.log_std = Variable(torch.ones(self.m) * init_log_std, requires_grad=True)
+        self.trainable_params = list(self.model.parameters()) + [self.log_std]
+        self.model.eval()
+
+        self.old_model = FCNetworkWithBatchNorm(self.n, self.m, hidden_sizes, nonlinearity, dropout, log_dir=log_dir)
+        self.old_log_std = Variable(torch.ones(self.m) * init_log_std)
+        self.old_params = list(self.old_model.parameters()) + [self.old_log_std]
+        for idx, param in enumerate(self.old_params):
+            param.data = self.trainable_params[idx].data.clone()
+        self.old_model.eval()
+
+        self.log_std_val = np.float64(self.log_std.data.numpy().ravel())
+        self.param_shapes = [p.data.numpy().shape for p in self.trainable_params]
+        self.param_sizes = [p.data.numpy().size for p in self.trainable_params]
+        self.d = np.sum(self.param_sizes)  # total number of params
+
+        self.obs_var = Variable(torch.randn(self.n), requires_grad=False)
+
+        # Register hooks to log activations
+        self.model.register_hooks()
+        self.old_model.register_hooks()
+        self.close_writer()
+
+    def get_action(self, observation):
+        o = np.float32(observation.reshape(1, -1))
+        self.obs_var.data = torch.from_numpy(o)
+        mean = self.model(self.obs_var).data.numpy().ravel()
+        noise = np.exp(self.log_std_val) * np.random.randn(self.m)
+        action = mean + noise
+        return [action, {'mean': mean, 'log_std': self.log_std_val, 'evaluation': mean}]
+
+    # Ensure to close the writer when done
+    def close_writer(self):
+        self.model.close_writer()
+        self.old_model.close_writer()
