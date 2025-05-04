8229da199b67381b2a33b3a39bfae6e7f09fb298
Sai Krishna Karnam <43415609+SaiKrishna1215@users.noreply.github.com>
2025-05-04 11:08:11 -0600

    Update gaussian_mlp.py

--- mjrl/policies/gaussian_mlp.py
+++ mjrl/policies/gaussian_mlp.py
@@ -51,8 +51,7 @@ class BiLSTMPolicy:
     def get_action(self, observation):
         o = np.float32(observation.reshape(1, -1))
         self.obs_var.data = torch.from_numpy(o)
-        with torch.no_grad():
-            mean = model(obs_var)[0].cpu().numpy().ravel()
+        mean = model(obs_var)[0].cpu().numpy().ravel()
         noise = np.exp(self.log_std_val) * np.random.randn(self.m)
         action = mean + noise * 10
         return [action, {'mean': mean, 'log_std': self.log_std_val, 'evaluation': mean}]
