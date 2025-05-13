dad258f9fc09aed7286a23007f9724306b9f0184
Sai Krishna Karnam <43415609+SaiKrishna1215@users.noreply.github.com>
2025-05-13 16:06:55 -0600

    Update gaussian_mlp.py

--- mjrl/policies/gaussian_mlp.py
+++ mjrl/policies/gaussian_mlp.py
@@ -6,6 +6,11 @@ from torch.autograd import Variable
 
 import torch.nn as nn
 
+import torch
+import torch.nn as nn
+import numpy as np
+from torch.autograd import Variable
+
 class BiLSTMPolicy:
     def __init__(self, env_spec,
                  lstm_hidden_size=64,
@@ -24,30 +29,32 @@ class BiLSTMPolicy:
             torch.manual_seed(seed)
             np.random.seed(seed)
 
-        self.lstm = nn.LSTM(input_size=self.n,
-                            hidden_size=lstm_hidden_size,
-                            batch_first=True,
-                            bidirectional=True)
-        self.fc = nn.Sequential(
-            nn.Linear(2 * lstm_hidden_size, mlp_hidden_size),
-            nn.Tanh(),
-            nn.Linear(mlp_hidden_size, self.m)
-        )
+        # Main model
+        self.model = BidirectionalLSTMNetwork(
+            obs_dim=self.n,
+            act_dim=self.m,
+            lstm_hidden_size=lstm_hidden_size,
+            mlp_hidden_size=mlp_hidden_size
+        ).to(self.device)
 
+        # Log std as trainable parameter
         self.log_std = nn.Parameter(torch.ones(self.m) * init_log_std)
-        self.trainable_params = list(self.lstm.parameters()) + list(self.fc.parameters()) + [self.log_std]
 
-        # Clone for old model
-        self.old_lstm = nn.LSTM(self.n, lstm_hidden_size, batch_first=True, bidirectional=True)
-        self.old_fc = nn.Sequential(
-            nn.Linear(2 * lstm_hidden_size, mlp_hidden_size),
-            nn.Tanh(),
-            nn.Linear(mlp_hidden_size, self.m)
-        )
+        # Parameter lists
+        self.trainable_params = list(self.model.parameters()) + [self.log_std]
+
+        # Clone old model
+        self.old_model = BidirectionalLSTMNetwork(
+            obs_dim=self.n,
+            act_dim=self.m,
+            lstm_hidden_size=lstm_hidden_size,
+            mlp_hidden_size=mlp_hidden_size
+        ).to(self.device)
         self.old_log_std = nn.Parameter(torch.ones(self.m) * init_log_std)
-        self.old_params = list(self.old_lstm.parameters()) + list(self.old_fc.parameters()) + [self.old_log_std]
+        self.old_params = list(self.old_model.parameters()) + [self.old_log_std]
         self._copy_params()
 
+        # Setup for saving/restoring parameters
         self.log_std_val = np.float64(self.log_std.detach().numpy().ravel())
         self.param_shapes = [p.shape for p in self.trainable_params]
         self.param_sizes = [p.numel() for p in self.trainable_params]
@@ -55,79 +62,58 @@ class BiLSTMPolicy:
         self.obs_var = Variable(torch.randn(self.n), requires_grad=False)
 
     def show_activations(self):
-        return self.lstm.activations
+        return self.model.activations
+
 
     def _copy_params(self):
         for idx, param in enumerate(self.old_params):
             param.data.copy_(self.trainable_params[idx].data.clone())
 
-    def forward_model(self, x, lstm_module, fc_module):
+    def forward_model(self, x, model):
         if x.dim() == 2:
             x = x.unsqueeze(1)
-        out, _ = lstm_module(x)
-        return fc_module(out[:, -1, :])
-
-    def get_param_values(self):
-        return np.concatenate([p.detach().cpu().view(-1).numpy() for p in self.trainable_params])
-
-    def set_param_values(self, new_params, set_new=True, set_old=True):
-        if set_new:
-            current_idx = 0
-            for i, param in enumerate(self.trainable_params):
-                vals = new_params[current_idx:current_idx + self.param_sizes[i]]
-                param.data = torch.tensor(vals.reshape(self.param_shapes[i]), dtype=torch.float32)
-                current_idx += self.param_sizes[i]
-            self.trainable_params[-1].data = torch.clamp(self.trainable_params[-1], self.min_log_std).data
-            self.log_std_val = np.float64(self.log_std.detach().numpy().ravel())
-        if set_old:
-            current_idx = 0
-            for i, param in enumerate(self.old_params):
-                vals = new_params[current_idx:current_idx + self.param_sizes[i]]
-                param.data = torch.tensor(vals.reshape(self.param_shapes[i]), dtype=torch.float32)
-                current_idx += self.param_sizes[i]
-            self.old_params[-1].data = torch.clamp(self.old_params[-1], self.min_log_std).data
+        out, _ = model(x)
+        return out
 
     def get_action(self, observation):
         o = np.float32(observation.reshape(1, -1))
-        self.obs_var.data = torch.from_numpy(o)
+        self.obs_var.data = torch.from_numpy(o).to(self.device)
         with torch.no_grad():
-            mean = self.forward_model(self.obs_var, self.lstm, self.fc)
+            mean = self.forward_model(self.obs_var, self.model)
         mean = mean.squeeze(0).cpu().numpy()
         noise = np.exp(self.log_std_val) * np.random.randn(self.m)
         action = mean + noise * 10
         return [action, {'mean': mean, 'log_std': self.log_std_val, 'evaluation': mean}]
 
-    def mean_LL(self, observations, actions, lstm_model=None, fc_model=None, log_std=None):
-        lstm_model = self.lstm if lstm_model is None else lstm_model
-        fc_model = self.fc if fc_model is None else fc_model
+    def mean_LL(self, observations, actions, model=None, log_std=None):
+        model = self.model if model is None else model
         log_std = self.log_std if log_std is None else log_std
 
         if type(observations) is not torch.Tensor:
-            obs_var = Variable(torch.from_numpy(observations).float(), requires_grad=False)
+            obs_var = Variable(torch.from_numpy(observations).float(), requires_grad=False).to(self.device)
         else:
-            obs_var = observations
-        if type(actions) is not torch.Tensor:
-            act_var = Variable(torch.from_numpy(actions).float(), requires_grad=False)
-        else:
-            act_var = actions
+            obs_var = observations.to(self.device)
 
-        mean = self.forward_model(obs_var, lstm_model, fc_model)
+        if type(actions) is not torch.Tensor:
+            act_var = Variable(torch.from_numpy(actions).float(), requires_grad=False).to(self.device)
+        else:
+            act_var = actions.to(self.device)
+
+        mean = self.forward_model(obs_var, model)
         zs = (act_var - mean) / torch.exp(log_std)
-        LL = - 0.5 * torch.sum(zs ** 2, dim=1) + \
-             - torch.sum(log_std) + \
-             - 0.5 * self.m * np.log(2 * np.pi)
+        LL = -0.5 * torch.sum(zs ** 2, dim=1) - torch.sum(log_std) - 0.5 * self.m * np.log(2 * np.pi)
         return mean, LL
 
-    def log_likelihood(self, observations, actions, lstm_model=None, fc_model=None, log_std=None):
-        mean, LL = self.mean_LL(observations, actions, lstm_model, fc_model, log_std)
+    def log_likelihood(self, observations, actions, model=None, log_std=None):
+        mean, LL = self.mean_LL(observations, actions, model, log_std)
         return LL.detach().cpu().numpy()
 
     def old_dist_info(self, observations, actions):
-        mean, LL = self.mean_LL(observations, actions, self.old_lstm, self.old_fc, self.old_log_std)
+        mean, LL = self.mean_LL(observations, actions, self.old_model, self.old_log_std)
         return [LL, mean, self.old_log_std]
 
     def new_dist_info(self, observations, actions):
-        mean, LL = self.mean_LL(observations, actions, self.lstm, self.fc, self.log_std)
+        mean, LL = self.mean_LL(observations, actions, self.model, self.log_std)
         return [LL, mean, self.log_std]
 
     def likelihood_ratio(self, new_dist_info, old_dist_info):
@@ -146,6 +132,28 @@ class BiLSTMPolicy:
         Dr = 2 * new_std ** 2 + 1e-8
         sample_kl = torch.sum(Nr / Dr + new_log_std - old_log_std, dim=1)
         return torch.mean(sample_kl)
+
+    def get_param_values(self):
+        return np.concatenate([p.detach().cpu().view(-1).numpy() for p in self.trainable_params])
+
+    def set_param_values(self, new_params, set_new=True, set_old=True):
+        if set_new:
+            current_idx = 0
+            for i, param in enumerate(self.trainable_params):
+                vals = new_params[current_idx:current_idx + self.param_sizes[i]]
+                param.data = torch.tensor(vals.reshape(self.param_shapes[i]), dtype=torch.float32).to(self.device)
+                current_idx += self.param_sizes[i]
+            self.trainable_params[-1].data = torch.clamp(self.trainable_params[-1], self.min_log_std).data
+            self.log_std_val = np.float64(self.log_std.detach().cpu().numpy().ravel())
+
+        if set_old:
+            current_idx = 0
+            for i, param in enumerate(self.old_params):
+                vals = new_params[current_idx:current_idx + self.param_sizes[i]]
+                param.data = torch.tensor(vals.reshape(self.param_shapes[i]), dtype=torch.float32).to(self.device)
+                current_idx += self.param_sizes[i]
+            self.old_params[-1].data = torch.clamp(self.old_params[-1], self.min_log_std).data
+
 
 
 
